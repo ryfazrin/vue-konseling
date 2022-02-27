193b8dc806151d86a1a334cfbad431977561f89f
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2022-02-27 11:03:59 +0800

    create: DashboardPage

---
+++ src/views/DashboardPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Dashboard</p>
+    <p>{{ $route.path }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DashboardPage'
+}
+</script>
+
+<style>
+
+</style>
