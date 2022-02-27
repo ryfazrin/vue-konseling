b7eafd1a0b284df42528b16b01419a68e621a318
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2022-02-27 09:48:55 +0800

    create: TheSidebar

---
+++ src/containers/_nav.js
@@ -0,0 +1,61 @@
+export default [
+  {
+    _name: 'CSidebarNav',
+    _children: [
+      {
+        _name: 'CSidebarNavItem',
+        name: 'Dashboard',
+        to: '/dashboard',
+        icon: 'cil-speedometer',
+        badge: {
+          color: 'primary',
+          text: 'NEW'
+        }
+      },
+      {
+        _name: 'CSidebarNavTitle',
+        _children: ['Theme']
+      },
+      {
+        _name: 'CSidebarNavItem',
+        name: 'Colors',
+        to: '/theme/colors',
+        icon: 'cil-drop'
+      },
+      {
+        _name: 'CSidebarNavItem',
+        name: 'Typography',
+        to: '/theme/typography',
+        icon: 'cil-pencil'
+      },
+      {
+        _name: 'CSidebarNavTitle',
+        _children: ['Components']
+      },
+      {
+        _name: 'CSidebarNavDropdown',
+        name: 'Buttons',
+        route: '/buttons',
+        icon: 'cil-cursor',
+        items: [
+          {
+            name: 'Buttons',
+            to: '/buttons/standard-buttons'
+          },
+          {
+            name: 'Button Dropdowns',
+            to: '/buttons/dropdowns'
+          },
+          {
+            name: 'Button Groups',
+            to: '/buttons/button-groups'
+          },
+          {
+            name: 'Brand Buttons',
+            to: '/buttons/brand-buttons'
+          }
+        ]
+      },
+    ]
+  }
+]
