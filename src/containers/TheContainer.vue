974302483d0e11379d90af3e6d9d23252be598d3
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2022-02-27 09:18:23 +0800

    create: TheContainer.vue

---
+++ src/containers/TheContainer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="c-app">
+
+    <CWrapper>
+      <TheHeader />
+      <div class="c-body">
+        <main class="c-main">
+          <CContainer fluid>
+            <transition name="fade" mode="out-in">
+              <!-- <router-view :key="$route.path"></router-view> -->
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis dolor quisquam, molestiae aliquid laudantium fugiat ad inventore aut molestias earum voluptates a sequi nesciunt deleniti ratione ut consequuntur eligendi necessitatibus!</p>
+            </transition>
+          </CContainer>
+        </main>
+      </div>
+      <TheFooter />
+    </CWrapper>
+  </div>
+</template>
+
+<script>
+import TheFooter from './TheFooter'
+
+export default {
+  name: 'TheContainer',
+  components: {
+    TheFooter
+  }
+}
+</script>
+
+<style>
+
+</style>
