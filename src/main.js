a667e1cec1359d66cc5cd36fdd8065c31584bbff
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2022-02-27 11:06:12 +0800

    update: register router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 import CoreuiVue from '@coreui/vue'
 
 Vue.config.productionTip = false
 Vue.use(CoreuiVue)
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
