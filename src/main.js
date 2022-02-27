b668920b4f1887fd5afb1a29b92b28c4c82cc08a
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2022-02-27 13:21:28 +0800

    add: coreui icons

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import CoreuiVue from '@coreui/vue'
+import { iconsSet as icons } from './assets/icons/icons.js'
 
 Vue.config.productionTip = false
 Vue.use(CoreuiVue)
 
 new Vue({
   router,
+  icons,
   render: h => h(App)
 }).$mount('#app')
