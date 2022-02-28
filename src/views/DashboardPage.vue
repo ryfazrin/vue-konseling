1b1834c9b8312eea91d81b333b3316ce3c07726f
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2022-02-28 10:09:20 +0800

    update: content Dashboard

--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -1,7 +1,24 @@
 <template>
   <div>
-    <p>Dashboard</p>
-    <p>{{ $route.path }}</p>
+    <CCard  accent-color="primary">
+      <CCardHeader>
+        Card title
+        <div class="card-header-actions">
+          <a 
+            href="https://coreui.io/vue/docs/components/card-components" 
+            class="card-header-action" 
+            rel="noreferrer noopener" 
+            target="_blank"
+          >
+            <small class="text-muted">docs</small>
+          </a>
+        </div>
+      </CCardHeader>
+      <CCardBody>
+        <p>Dashboard</p>
+        <p>{{ $route.path }}</p>
+      </CCardBody>
+    </CCard>
   </div>
 </template>
 
